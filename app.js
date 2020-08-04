0020e3d47bbbfdfa2d06a000fb730da157ad88bc
Gamygams1234 <gamyburgos@gmail.com>
2020-08-04 14:24:25 -0700

    adding the ejs and working with the ejs

--- app.js
+++ app.js
@@ -6,24 +6,35 @@ const app = express();
 // listen for requests
 app.listen(3000);
 
+// register view view engine
+// the default is going to be the views folder
+app.set("view engine", "ejs");
+
+// this would be if we wanted to change the views folder
+//app.set("views", "myviews")
+
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+
 app.get("/", (req, res) => {
-  //res.send("<p>home page</p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    { title: "Yoshi finds eggs", snippet: "Lorem ipsum dolor sit amet consectetur" },
+    { title: "Mario finds stars", snippet: "Lorem ipsum dolor sit amet consectetur" },
+    { title: "How to defeat bowser", snippet: "Lorem ipsum dolor sit amet consectetur" },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  //res.send("<p>about page</p>");
-  // where is it relative to which is the dirname from the begining
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-// redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
 });
 
 // 404 page
 app.use((req, res) => {
-  // it has to know that it is a 404 error
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
