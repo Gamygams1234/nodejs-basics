8e3b97d10735f64498565db82fe293bd91c74d6e
Gamygams1234 <gamyburgos@gmail.com>
2020-08-04 00:27:20 -0700

    The response object

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ const http = require("http");
 // the server is stored
 const server = http.createServer((req, res) => {
   // this will show the path, and the method
-  console.log(req.url, req.method);
+  console.log(req.url);
+
+  // set header content type
+
+  // this will deliver the plain text
+
+  // this will deliver in html form
+  // res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Content-Type", "text/html");
+  res.write("<h1>Hello world</h1>");
+  res.end();
 });
 
 // localhost is the default value for 2nd argument
