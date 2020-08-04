63e93d13377309085f39cb77dc4a07249f29ab34
Gamygams1234 <gamyburgos@gmail.com>
2020-08-03 22:18:55 -0700

    creating a server

---
+++ server.js
@@ -0,0 +1,12 @@
+// starting the HTTP module
+const http = require("http");
+
+// the server is stored
+const server = http.createServer((req, res) => {
+  console.log("request made");
+});
+
+// localhost is the default value for 2nd argument
+server.listen(3000, "localhost", () => {
+  console.log("listening for requests on port 3000");
+});
--- test.js
+++
@@ -1,4 +0,0 @@
-// this is the directory name in the file
-console.log(__dirname);
-// this is the directory name with the file attatched
-console.log(__filename);
